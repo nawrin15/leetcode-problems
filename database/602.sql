01fefaaabdd015fc48f1db0975dc6f4ce4940da9
nawrin15 <sadianawrin93@gmail.com>
2023-12-10 08:08:15 +0600

    update

---
+++ database/602.sql
@@ -0,0 +1,10 @@
+----602. Friend Requests II: Who Has the Most Friends
+select id1 as id, count(id2) as num
+from
+(select requester_id as id1, accepter_id as id2 
+from RequestAccepted
+union
+select accepter_id as id1, requester_id as id2 
+from RequestAccepted) tmp1
+group by id1 
+order by num desc limit 1
---
+++ database/626.sql
@@ -0,0 +1,9 @@
+----626. Exchange Seats
+select  
+      case 
+        when id % 2 = 0 then id - 1
+        when id % 2 = 1 and id < (select count(*) from seat)then id + 1
+        else id
+      end as id, 
+    student from seat
+    order by id
