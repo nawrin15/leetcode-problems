2be554afb7225d49f841c488c6773bbeeb3f3ea2
nawrin15 <sadianawrin93@gmail.com>
2024-01-02 10:34:12 +0600

    new code added

---
+++ sliding_window/345.Reverse_Vowels_of_a_String.py
@@ -0,0 +1,19 @@
+def reverseVowels(self, s: str) -> str:
+    sList = list(s)
+    start = 0
+    end = len(sList) - 1
+    vowels = 'aeiouAEIOU'
+    while start < end:
+        if sList[start] in vowels and sList[end] in vowels:
+            temp = sList[start]
+            sList[start] = sList[end]
+            sList[end] = temp
+            end -= 1
+            start += 1
+        elif sList[start] not in vowels:
+            start += 1
+        elif sList[end] not in vowels:
+            end -= 1
+    return "".join(sList)
+
+print(reverseVowels('leetcode'))
